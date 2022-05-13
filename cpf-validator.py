fdfb0a5c0eb7c87917fba3e715436727a4cc869a
Carlos Szpiro <90582342+carlosszpiro@users.noreply.github.com>
2022-05-13 16:01:00 -0300

    Update cpf-validator.py

--- cpf-validator.py
+++ cpf-validator.py
@@ -9,22 +9,26 @@ else:
     print('Um CPF possui somente 11 números!')
 
   else:
-    cpf = list(str(user_cpf))
-    digito_1 = 0
-    digito_2 = 0
-
-    for i, i2 in enumerate(range(10, 1, -1)):
-      digito_1 += int(cpf[i]) * i2
-
-    digito_1 = 11 - (digito_1 % 11) if 11 - (digito_1 % 11) < 10 else 0
-
-    for i, i2 in enumerate(range(11, 1, -1)):
-      digito_2 += int(cpf[i]) * i2
-
-    digito_2 = 11 - (digito_2 % 11) if 11 - (digito_2 % 11) < 10 else 0
-
-    if int(cpf[9]) == digito_1 and int(cpf[10]) == digito_2:
-      print('O CPF', user_cpf, 'é válido!')
+    if user_cpf == (user_cpf[0] * 11):
+      print('O CPF é inválido!')
 
     else:
-      print('O CPF', user_cpf, 'é inválido!')
+      cpf = list(str(user_cpf))
+      digito_1 = 0
+      digito_2 = 0
+
+      for i, i2 in enumerate(range(10, 1, -1)):
+        digito_1 += int(cpf[i]) * i2
+
+      digito_1 = 11 - (digito_1 % 11) if 11 - (digito_1 % 11) < 10 else 0
+
+      for i, i2 in enumerate(range(11, 1, -1)):
+        digito_2 += int(cpf[i]) * i2
+
+      digito_2 = 11 - (digito_2 % 11) if 11 - (digito_2 % 11) < 10 else 0
+
+      if int(cpf[9]) == digito_1 and int(cpf[10]) == digito_2:
+        print('O CPF', user_cpf, 'é válido!')
+
+      else:
+        print('O CPF', user_cpf, 'é inválido!')
